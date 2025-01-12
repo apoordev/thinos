5d7362f2fddc408acb1de5cdee7f5c467485d631
Barrett Williamson <williamsonbarrett@gmail.com>
2025-01-12 11:45:41 -0500

    feat: add tailscale

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -ouex pipefail
 
-dnf install -y tmux 
+curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/tailscale.repo
+
+dnf install -y tmux tailscale
 
 systemctl enable podman.socket
+systemctl disable tailscaled.service
