f138af509a86eac4a4b2c9c27a11f1f30290ccd5
Barrett Williamson <williamsonbarrett@gmail.com>
2025-02-03 15:50:50 -0500

    feat: install KDE DE

--- build.sh
+++ build.sh
@@ -5,8 +5,33 @@ set -ouex pipefail
 MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID | cut -d'.' -f1')"
 export MAJOR_VERSION_NUMBER
 
+dnf -y update
+
+dnf config-manager --set-enabled crb
+dnf -y install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$MAJOR_VERSION_NUMBER.noarch.rpm"
+
+
 # Install VDI Tools
 dnf install -y tmux virt-viewer
+
+# Multimidia codecs
+dnf -y install @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl
+
+dnf group install -y --nobest \
+	-x rsyslog* \
+	-x cockpit \
+	-x cronie* \
+	-x crontabs \
+	-x PackageKit \
+	-x PackageKit-command-not-found \
+	"Common NetworkManager submodules" \
+	"Core" \
+	"Fonts" \
+	"Guest Desktop Agents" \
+	"Hardware Support" \
+	"Printing Client" \
+	"Standard" \
+	"KDE Plasma Workspaces" \
 
 # Install Tailscale
 #curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
@@ -14,8 +39,6 @@ dnf config-manager --add-repo "https://pkgs.tailscale.com/stable/centos/${MAJOR_
 dnf config-manager --set-disabled tailscale-stable
 dnf -y --enablerepo tailscale-stable install \
 	tailscale
-
-dnf group install -y --nobest 'KDE'
 
 dnf -y install \
 	plymouth \
